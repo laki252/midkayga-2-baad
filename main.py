7fc7613470fd47af270f8608bd7c8feceef950b4
laki252 <lakicalinuur@icloud.com>
2025-11-16 13:28:34 +0300

    Update main.py

--- main.py
+++ main.py
@@ -480,7 +480,7 @@ async def clean_up_callback(client, callback_query: CallbackQuery):
         uid = callback_query.from_user.id
         lang = get_user_lang(uid, "en")
         mode = get_user_mode(uid, "📄 Text File")
-        instruction = f"Clean and normalize this transcription into (lang={lang}). Remove ASR artifacts like [inaudible], repeated words, filler noises, timestamps, and incorrect punctuation. Produce a clean, well-punctuated, readable text  Do not add introductions or explanations."
+        instruction = f"Clean and normalize this transcription Remove ASR artifacts like [inaudible], repeated words, filler noises, timestamps, and incorrect punctuation. Produce a clean, well-punctuated, readable text Also, translate the text into (lang={lang}).If translatin is Somali, write the numbers in the Somali style, like 2025 as 'labada kun labaatan iyo shantii,' and translate it to be easily understood and simpler Do not add introductions or explanations."
         cleaned_text = await loop.run_in_executor(None, ask_gemini, stored_text, instruction)
         if not cleaned_text:
             await client.send_message(chat_id, "No cleaned text returned.", reply_to_message_id=orig_msg_id)
